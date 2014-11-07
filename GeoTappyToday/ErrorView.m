fb97c16ea469e7b2c10f235d304530f89299ad3d
Dylan Marriott <info@d-32.com>
2014-11-07 15:13:33 -0500

    Added icon for error (push / widget)

--- GeoTappyToday/ErrorView.m
+++ GeoTappyToday/ErrorView.m
@@ -13,7 +13,7 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
         UIImageView* cross = [[UIImageView alloc] initWithFrame:CGRectMake(self.frame.size.width / 2 - 10, self.frame.size.height / 2 - 10, 20, 20)];
-        cross.image = [[UIImage imageNamed:@"tick"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+        cross.image = [[UIImage imageNamed:@"cross"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
         cross.tintColor = [UIColor whiteColor];
         [self addSubview:cross];
         self.backgroundColor = [UIColor colorWithRed:0.64 green:0.00 blue:0.00 alpha:1.00];
