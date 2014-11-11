c9f4e4491eb9bd3f7aa7b45ab49ccc1403720843
Dylan Marriott <info@d-32.com>
2014-11-12 00:47:18 +0100

    Added OS X notification to build script, just for fun :)

--- build_hockey.sh
+++ build_hockey.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Building GeoTappy Hockey"
 ipa build -s GeoTappy -m Provisioning/GeoTappyAdHoc.mobileprovision -d build/HockeyApp/
+osascript -e 'display notification "Build for HockeyApp completed." with title "GeoTappy" sound name "Glass"'
 echo "Done"
