b50b45bce503075add635213325295c8ff0e616d
Dylan Marriott <info@d-32.com>
2014-11-23 02:30:25 +0100

    Added more tests

--- GeoTappyTests/GeoTappyTests.m
+++ GeoTappyTests/GeoTappyTests.m
@@ -69,7 +69,6 @@
 }
 
 - (void)testNoUpdates {
-    
     NSString* jsonString = @"{ \"profile\":{ \"id\":0, \"email\":\"\", \"name\":\"Dylan Marriott\", \"profile_photo_url\":\"p0\", \"friends\":[ { \"id\":10, \"email\":\"\", \"name\":\"Friend #10\", \"profile_photo_url\":\"p10\" }, { \"id\":11, \"email\":\"\", \"name\":\"Friend #11\", \"profile_photo_url\":\"p11\" }, { \"id\":12, \"email\":\"\", \"name\":\"Friend #12\", \"profile_photo_url\":\"p12\" }, { \"id\":13, \"email\":\"\", \"name\":\"Friend #13\", \"profile_photo_url\":\"p13\" }, { \"id\":14, \"email\":\"\", \"name\":\"Friend #14\", \"profile_photo_url\":\"p14\" }, { \"id\":15, \"email\":\"\", \"name\":\"Friend #15\", \"profile_photo_url\":\"p15\" }, { \"id\":16, \"email\":\"\", \"name\":\"Friend #16\", \"profile_photo_url\":\"p16\" }, { \"id\":17, \"email\":\"\", \"name\":\"Friend #17\", \"profile_photo_url\":\"p17\" }, { \"id\":18, \"email\":\"\", \"name\":\"Friend #18\", \"profile_photo_url\":\"p18\" }, { \"id\":19, \"email\":\"\", \"name\":\"Friend #19\", \"profile_photo_url\":\"p19\" }, ], \"cover_photo_url\":\"\" } }";
     
     NSDictionary* json = [NSJSONSerialization JSONObjectWithData:[jsonString dataUsingEncoding:NSUTF8StringEncoding] options:0 error:nil];
@@ -91,6 +90,65 @@
     XCTAssert(fav1.identifier.intValue == 15);
     XCTAssert(fav2.identifier.intValue == 18);
     XCTAssert([fav3.name isEqualToString:@"Group 1"]);
+    XCTAssert(fav3.friends.count == 4);
+}
+
+- (void)testNewOrder {
+    NSString* jsonString = @"{ \"profile\":{ \"id\":0, \"email\":\"\", \"name\":\"Dylan Marriott\", \"profile_photo_url\":\"p0\", \"friends\":[ { \"id\":10, \"email\":\"\", \"name\":\"Friend #10\", \"profile_photo_url\":\"p10\" }, { \"id\":16, \"email\":\"\", \"name\":\"Friend #16\", \"profile_photo_url\":\"p16\" }, { \"id\":11, \"email\":\"\", \"name\":\"Friend #11\", \"profile_photo_url\":\"p11\" }, { \"id\":13, \"email\":\"\", \"name\":\"Friend #13\", \"profile_photo_url\":\"p13\" }, { \"id\":19, \"email\":\"\", \"name\":\"Friend #19\", \"profile_photo_url\":\"p19\" }, { \"id\":15, \"email\":\"\", \"name\":\"Friend #15\", \"profile_photo_url\":\"p15\" }, { \"id\":12, \"email\":\"\", \"name\":\"Friend #12\", \"profile_photo_url\":\"p12\" }, { \"id\":17, \"email\":\"\", \"name\":\"Friend #17\", \"profile_photo_url\":\"p17\" }, { \"id\":18, \"email\":\"\", \"name\":\"Friend #18\", \"profile_photo_url\":\"p18\" }, { \"id\":14, \"email\":\"\", \"name\":\"Friend #14\", \"profile_photo_url\":\"p14\" }, ], \"cover_photo_url\":\"\" } }";
+    
+    NSDictionary* json = [NSJSONSerialization JSONObjectWithData:[jsonString dataUsingEncoding:NSUTF8StringEncoding] options:0 error:nil];
+    [_user updateWithJson:json];
+    
+    XCTAssert(_user.selectedFavourites.count == 4);
+    XCTAssert(_user.unselectedFavourites.count == 8);
+    XCTAssert(_user.friends.count == 10);
+    
+    XCTAssert(_user.identifier.intValue == 0);
+    XCTAssert([_user.profileImageUrl isEqualToString:@"p0"]);
+    
+    User* fav0 = _user.selectedFavourites[0];
+    User* fav1 = _user.selectedFavourites[1];
+    User* fav2 = _user.selectedFavourites[2];
+    Group* fav3 = _user.selectedFavourites[3];
+    
+    XCTAssert(fav0.identifier.intValue == 10);
+    XCTAssert(fav1.identifier.intValue == 15);
+    XCTAssert(fav2.identifier.intValue == 18);
+    XCTAssert([fav3.name isEqualToString:@"Group 1"]);
+    XCTAssert(fav3.friends.count == 4);
+}
+
+- (void)testAddAndRemove {
+    NSString* jsonString = @"{ \"profile\":{ \"id\":0, \"email\":\"\", \"name\":\"Dylan Marriott\", \"profile_photo_url\":\"p0\", \"friends\":[ { \"id\":10, \"email\":\"\", \"name\":\"Friend #10\", \"profile_photo_url\":\"p10\" }, { \"id\":16, \"email\":\"\", \"name\":\"Friend #16\", \"profile_photo_url\":\"p16\" }, { \"id\":11, \"email\":\"\", \"name\":\"Friend #11\", \"profile_photo_url\":\"p11\" }, { \"id\":13, \"email\":\"\", \"name\":\"Friend #13\", \"profile_photo_url\":\"p13\" }, { \"id\":19, \"email\":\"\", \"name\":\"Friend #19\", \"profile_photo_url\":\"p19\" }, { \"id\":15, \"email\":\"\", \"name\":\"Friend #15\", \"profile_photo_url\":\"p15\" }, { \"id\":17, \"email\":\"\", \"name\":\"Friend #17\", \"profile_photo_url\":\"p17\" }, { \"id\":24, \"email\":\"\", \"name\":\"Friend #24\", \"profile_photo_url\":\"p24\" }, ], \"cover_photo_url\":\"\" } }";
+    
+    NSDictionary* json = [NSJSONSerialization JSONObjectWithData:[jsonString dataUsingEncoding:NSUTF8StringEncoding] options:0 error:nil];
+    [_user updateWithJson:json];
+    
+    XCTAssert(_user.selectedFavourites.count == 3);
+    XCTAssert(_user.unselectedFavourites.count == 7);
+    XCTAssert(_user.friends.count == 8);
+    
+    XCTAssert(_user.identifier.intValue == 0);
+    XCTAssert([_user.profileImageUrl isEqualToString:@"p0"]);
+    
+    User* fav0 = _user.selectedFavourites[0];
+    User* fav1 = _user.selectedFavourites[1];
+    Group* fav3 = _user.selectedFavourites[2];
+    
+    XCTAssert(fav0.identifier.intValue == 10);
+    XCTAssert(fav1.identifier.intValue == 15);
+    XCTAssert([fav3.name isEqualToString:@"Group 1"]);
+    XCTAssert(fav3.friends.count == 3);
+}
+
+- (void)testUpdateProfilePhotos {
+    NSString* jsonString = @"{ \"profile\":{ \"id\":0, \"email\":\"\", \"name\":\"Dylan Marriott\", \"profile_photo_url\":\"new1\", \"friends\":[ { \"id\":10, \"email\":\"\", \"name\":\"Friend #10\", \"profile_photo_url\":\"p10\" }, { \"id\":11, \"email\":\"\", \"name\":\"Friend #11\", \"profile_photo_url\":\"p11\" }, { \"id\":12, \"email\":\"\", \"name\":\"Friend #12\", \"profile_photo_url\":\"p12\" }, { \"id\":13, \"email\":\"\", \"name\":\"Friend #13\", \"profile_photo_url\":\"p13\" }, { \"id\":14, \"email\":\"\", \"name\":\"Friend #14\", \"profile_photo_url\":\"p14\" }, { \"id\":15, \"email\":\"\", \"name\":\"Friend #15\", \"profile_photo_url\":\"new2\" }, { \"id\":16, \"email\":\"\", \"name\":\"Friend #16\", \"profile_photo_url\":\"p16\" }, { \"id\":17, \"email\":\"\", \"name\":\"Friend #17\", \"profile_photo_url\":\"p17\" }, { \"id\":18, \"email\":\"\", \"name\":\"Friend #18\", \"profile_photo_url\":\"p18\" }, { \"id\":19, \"email\":\"\", \"name\":\"Friend #19\", \"profile_photo_url\":\"p19\" }, ], \"cover_photo_url\":\"\" } }";
+    
+    NSDictionary* json = [NSJSONSerialization JSONObjectWithData:[jsonString dataUsingEncoding:NSUTF8StringEncoding] options:0 error:nil];
+    [_user updateWithJson:json];
+    
+    XCTAssert([_user.profileImageUrl isEqualToString:@"new1"]);
+    XCTAssert([[_user.selectedFavourites[1] profileImageUrl] isEqualToString:@"new2"]);
 }
 
 @end
